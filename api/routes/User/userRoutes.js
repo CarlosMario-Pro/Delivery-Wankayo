cc5202d692fe869ebebef5c10e70cf2c0b4c635e
MarioReyes <cmario.reyesp@gmail.com>
2023-05-18 11:41:09 -0500

    feature: Se crea la ruta de login, register y recuperación de contraseña

--- api/routes/User/userRoutes.js
+++ api/routes/User/userRoutes.js
@@ -7,17 +7,19 @@ const { isUser, isAdmin, isSuperAdmin } = require("../../middlewares/auth");
 //http://localhost:3001/user
 //REGISTRO - USUARIOS
 userRouter.post("/register", user.registerUsers);                               //POST para registrar usuarios
+userRouter.put("/unblockUser/:idUser", user.unblockUser);                       //PUT para desbloquear un usuario
 userRouter.delete("/deleteAccount/:idUser", user.deleteAccountUser);            //POST para registrar usuarios
 // userRouter.get("/verify/:token", user.confirmUser);                             //GET para verificar la cuenta del usuario recién registrado
 userRouter.put("/logicalDeletionUser/:idUser", user.logicalDeletionUser);       //DELETE para aplicar eliminado lógico a cuenta del usuario
-userRouter.put("/changePasswordUser/:idUser", user.changePasswordUser);         //PUT para cambiar contraseña del usuario
+userRouter.put("/recoverPassword/:idUser", user.recoverPassword);               //PUT para cambiar contraseña del usuario
+userRouter.put("/changePasswordr", user.changePasswordr);         //PUT para cambiar contraseña del usuario
 
 
 //INFORMATION
 userRouter.get("/getAllUsers", user.getAllUsers);                               //GET para obtener todos los usuarios con role "user"
-userRouter.get("/getAllEmails", user.getAllEmails);                               //GET para obtener todos los usuarios con role "user"
+userRouter.get("/getAllEmails", user.getAllEmails);                             //GET para obtener todos los usuarios con role "user"
 userRouter.get("/getAllInfoUser/:idUser", user.getAllInfoUser);                 //GET para obtener toda la información personal del usuario
-userRouter.put("/putUserInfo/:idUser", user.putUserInfo);                               //PUT para modificar información personal del usuario
+userRouter.put("/putUserInfo/:idUser", user.putUserInfo);                       //PUT para modificar información personal del usuario
 
 
 //ADDRESS
@@ -29,10 +31,10 @@ userRouter.delete("/deleteAddress/:idAddress", user.deleteAddress);             
 
 
 //ORDERS
-userRouter.get("/getIdOrders/:idOrder", user.getIdOrders);                      //POST para crear una order
-userRouter.get("/getIdUserOrders/:idUser", user.getIdUserOrders);               //POST para crear una order
-userRouter.post("/postOrders/:idUser", user.postOrders);                        //GET para obtener order actual del usuarios por su ID
-userRouter.get("/getUserOrdersDelivered/:idUser", user.getUserOrdersDelivered);     //GET para obtener order el historial de órdenes del usuario
+userRouter.get("/getIdOrders/:idOrder", user.getIdOrders);                                                  //POST para crear una order
+userRouter.get("/getIdUserOrders/:idUser", user.getIdUserOrders);                                           //POST para crear una order
+userRouter.post("/postOrders/:idUser", user.postOrders);                                                    //GET para obtener order actual del usuarios por su ID
+userRouter.get("/getUserOrdersDelivered/:idUser", user.getUserOrdersDelivered);                             //GET para obtener order el historial de órdenes del usuario
 userRouter.get("/getUserOrdersActivesPending/:idUser", user.getUserOrdersActivesPending);                   //GET para obtener order el historial de órdenes del usuario
 userRouter.get("/getUserOrdersActivesInPreparation/:idUser", user.getUserOrdersActivesInPreparation);       //GET para obtener order el historial de órdenes del usuario
 userRouter.get("/getUserOrdersActivesOnTheWay/:idUser", user.getUserOrdersActivesOnTheWay);                 //GET para obtener order el historial de órdenes del usuario
